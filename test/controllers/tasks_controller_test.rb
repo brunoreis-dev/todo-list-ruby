6f27ee069e35612685c15c512be3b237e250350f
brunoreis-dev <b.macedoreis@icloud.com>
2024-02-27 14:11:34 -0300

    feat(test): Create controller and model tests

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,7 +1,31 @@
 require "test_helper"
 
 class TasksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @task = tasks(:validTask)
+  end
+
+  test "should get index" do
+    get tasks_url
+    assert_response :success
+  end
+
+  test "should create task" do
+    assert_difference('Task.count') do
+      post tasks_url, params: { task: @task.task }
+    end
+    assert_response :success
+  end
+
+  test "should update task" do
+    patch task_url(@task), params: { task: { completed: @task.completed, task: @task.task } }
+    assert_response :success
+  end
+
+  test "should destroy task" do
+    assert_difference('Task.count', -1) do
+      delete task_url(@task)
+    end
+    assert_response :no_content
+  end
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid task with task name length greater than or equal to 3 characters" do
+    task = tasks(:validTask)
+    assert task.valid?
+  end
+
+  test "invalid task without task name" do
+    task = tasks(:taskWithoutName)
+    refute task.valid?, 'task is not valid without a task name'
+    assert_not_nil task.errors[:task], 'no validation error for task name present'
+  end
+
+  test "invalid task with task name length less than 3 characters" do
+    task = tasks(:taskWithShortName)
+    refute task.valid?, 'task is not valid with length less than 3 characters'
+    assert_not_nil task.errors[:task], 'no validation error for task name length'
+  end
 end
