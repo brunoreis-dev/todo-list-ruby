d79e0c3a76cce25b91945d182d86296c62235011
brunoreis-dev <b.macedoreis@icloud.com>
2024-02-27 13:43:06 -0300

    feat(controller): Implements create, update and delete routes and implements completed filter in list all route

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,46 @@
-class TasksController < ApplicationController=
+class TasksController < ApplicationController
+  before_action :set_task, only: [:update, :destroy]
+
   def index
-    @tasks = Task.all
+    if params[:completed].present?
+      @tasks = Task.where(completed: params[:completed])
+    else
+      @tasks = Task.all
+    end
     render json: @tasks
   end
 
   def create
-    render plain: "Hello, world! :Create"
+    @task = Task.new(task_params.merge(completed: false))
+
+    if @task.save
+      render json: @task, status: :created
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
   end
 
   def update
-    render plain: "Hello, world! :Update"
+    if @task.update(task_params)
+      render json: @task, status: :ok
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
-    render plain: "Hello, world! :Destroy"
+    @task.destroy
+    head :no_content
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def task_params
+      params.permit(:task, :completed)
+    end
 end
